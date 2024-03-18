67ab70c60519e68583d39200d79ae2791b31ee3c
Brina Shong <shongwtb@i2r.a-star.edu.sg>
2024-03-18 23:40:36 +0800

    wip: add gaussian noise and dynamic reconfigure

--- include/human_tracking_particle_filter/human_tracking_particle_filter.hpp
+++ include/human_tracking_particle_filter/human_tracking_particle_filter.hpp
@@ -1,17 +1,32 @@
 #ifndef HUMAN_TRACKING_PARTICLE_FILTER
 #define HUMAN_TRACKING_PARTICLE_FILTER
 
+#include <vector>
+#include <random>
+
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
+#include <dynamic_reconfigure/server.h>
 
+#include "human_tracking_particle_filter/particleFilterConfig.h"
 #include "pedsim_msgs/SemanticData.h"
 
 class HumanTrackingParticleFilter
 {
+  struct Pose
+  {
+    Pose(const double x, const double y)
+      : x{x}, y{y}
+    {}
+
+    double x, y;
+  };
+
   public:
     HumanTrackingParticleFilter(ros::NodeHandle &nh, ros::NodeHandle &pnh);
     ~HumanTrackingParticleFilter() {}
 
+    void reconfigureCB(human_tracking_particle_filter::particleFilterConfig &config);
     void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr &input);
     void humanPositionCallback(const pedsim_msgs::SemanticData::ConstPtr &input);
 
@@ -20,10 +35,19 @@ class HumanTrackingParticleFilter
     ros::Subscriber scan_sub_;
     ros::Subscriber human_sub_;
 
+    // dynamic reconfigure
+    std::shared_ptr<dynamic_reconfigure::Server<human_tracking_particle_filter::particleFilterConfig>> dyn_srv_;
+
     // params
     double frequency_;
     std::string scan_topic_;
     std::string human_topic_;
+    bool add_noise_;
+    double mean_;
+    double std_dev_;
+
+    // variables
+    std::vector<Pose> humans_;
 };
 
 #endif /* HUMAN_TRACKING_PARTICLE_FILTER */
--- src/human_tracking_particle_filter.cpp
+++ src/human_tracking_particle_filter.cpp
@@ -1,14 +1,30 @@
 #include <memory>
 #include "human_tracking_particle_filter/human_tracking_particle_filter.hpp"
+#include "human_tracking_particle_filter/particleFilterConfig.h"
 
 HumanTrackingParticleFilter::HumanTrackingParticleFilter(ros::NodeHandle &nh, ros::NodeHandle &pnh)
 {
+  // Initialize dynamic reconfigure server
+  dyn_srv_ = std::make_shared<dynamic_reconfigure::Server<human_tracking_particle_filter::particleFilterConfig>>();
+  dyn_srv_->setCallback([this](human_tracking_particle_filter::particleFilterConfig& config, ...){this->reconfigureCB(config);});
+
   pnh.param<double>("frequency", frequency_, 1.0);
   pnh.param<std::string>("scan_topic", scan_topic_, "/scan");
   pnh.param<std::string>("human_topic", human_topic_, "/human");
+  pnh.param<bool>("add_noise", add_noise_, false);
+  pnh.param<double>("mean", mean_, 0.0);
+  pnh.param<double>("std_dev", std_dev_, 1.0);
 
   scan_sub_ = nh.subscribe<sensor_msgs::LaserScan>(scan_topic_, 1, [this](const sensor_msgs::LaserScan::ConstPtr& input){ this->laserScanCallback(input); });
   human_sub_ = nh.subscribe<pedsim_msgs::SemanticData>(human_topic_, 1, [this](const pedsim_msgs::SemanticData::ConstPtr& input){ this->humanPositionCallback(input); });
+}
+
+void HumanTrackingParticleFilter::reconfigureCB(human_tracking_particle_filter::particleFilterConfig &config)
+{
+  this->add_noise_ = config.add_noise;
+  this->frequency_ = config.frequency;
+  this->mean_ = config.mean;
+  this->std_dev_ = config.std_dev;
 }
 
 void HumanTrackingParticleFilter::laserScanCallback(const sensor_msgs::LaserScan::ConstPtr &input)
@@ -19,6 +35,26 @@ void HumanTrackingParticleFilter::laserScanCallback(const sensor_msgs::LaserScan
 void HumanTrackingParticleFilter::humanPositionCallback(const pedsim_msgs::SemanticData::ConstPtr &input)
 {
   ROS_INFO("Human position received!");
+
+  // store the human positions
+  humans_.clear();
+  for (const auto &p : input->points)
+  {
+    humans_.emplace_back(p.location.x, p.location.y);
+  }
+
+  // apply Gaussian noise 
+  if (add_noise_)
+  {
+    std::default_random_engine rand_gen;
+    std::normal_distribution<double> gaussian(mean_, std_dev_);
+
+    for (auto &p : humans_)
+    {
+      p.x += gaussian(rand_gen);
+      p.y += gaussian(rand_gen);
+    }
+  }
 }
 
 int main(int argc, char** argv)
